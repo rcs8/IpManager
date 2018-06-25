6552044f72a41716ac4f16ad1368158722ab9dd6
Rodrigo Cunha <rcs8@cin.ufpe.br>
2018-06-25 17:06:49 -0300

    Added averages in aluno for testing

--- ipm-gui/src/app/aluno/aluno.ts
+++ ipm-gui/src/app/aluno/aluno.ts
@@ -2,42 +2,105 @@ export class Aluno{
   id: number;
   loginCin: string;
   loginHuxley: string;
-  notas: Map<string,number>;
-  faltas: number;
   senha: string;
+  listas: Map<string,number>;
+  provas: Map<string,number>;
+  miniprojeto: number;
+  final: number;
+  faltas: number;
+
 
   constructor(){
     this.clean();
   }
 
   clean(){
-    this.id = "";
+    this.id = 0;
     this.loginCin = "";
     this.loginHuxley = "";
-    this.notas = new Map<string,number>();
+    this.senha = "";
+    this.listas = new Map<string,number>();
+    this.provas = new Map<string,number>();
+    this.miniprojeto = 0;
+    this.final = 0;
     this.faltas = 0;
   }
 
   clone():Aluno{
     var aluno: Aluno = new Aluno();
-    aluno.notas = new Map<string,number>();
+    aluno.listas = new Map<string,number>();
+    aluno.provas = new Map<string,number>();
     aluno.copyFrom(this);
     return aluno;
   }
 
   copyFrom(from: Aluno): void {
-    this.nome = from.nome;
+    this.id = from.nome;
     this.loginCin = from.loginCin;
     this.loginHuxley = from.loginHuxley;
     this.senha = from.senha;
+    this.copyNotasFrom(from);
     this.faltas = from.faltas;
-    this.copyNotasFrom(from.notas);
   }
 
-  copyNotasFrom(from: Map<string,number>): void {
-    this.notas = new Map<string,number>();
-    for(let index in from){
-      this.notas[index] = from[index];
+  copyNotasFrom(from: Aluno): void {
+    this.listas = new Map<string,number>();
+    this.provas = new Map<string,number>();
+    for(let key in from.listas){
+      this.listas[key] = from.listas[key];
     }
+
+    for(let key in from.provas){
+      this.provas[key] = from.provas[key];
+    }
+    this.miniprojeto = from.miniprojeto;
+    this.final = from.final
+  }
+
+  getAverage(): number{
+    var average : number = 0;
+    for(let key in this.listas){
+      average = average + (this.listas[key] * 2)
+    }
+    for(let key in this.provas){
+      average = average + (this.provas[key] * 7)
+    }
+    average = average + (miniprojeto * 1);
+
+    return average / 34;
+  }
+
+  getFinalAverage(): number{
+    var finalAverage : number = 0;
+    var average : number = this.getAverage();
+    finalAverage = average + this.final / 2;
+    return finalAverage;
+  }
+
+  getAbsence(classes: number) : number {
+    var absence : number = 0;
+    absence = this.faltas / classes;
+    return absence * 100;
+  }
+
+  getStatus(classes: number) : string {
+    var status : string = "";
+    var absence : number = getAbsence(classes);
+    if(abscence > 25){
+      status = "Reprovado por Falta";
+    } else {
+      var average : number = getAverage();
+      if(average > 7) {
+        status = "Aprovado por média";
+      } else {
+        var finalAverage : number = getFinalAverage();
+        if(finalAverage > 5){
+          status = "Aprovado";
+        } else {
+          status = "Reprovado";
+        }
+      }
+    }
+    return status;
   }
 }
