6e7563776ae293811d69ce416c8eaec46895a10c
Rodrigo Cunha <rcs8@cin.ufpe.br>
2018-06-25 02:08:27 -0300

    Added GUI and Routing for alunos

---
+++ ipm-gui/src/app/aluno/aluno-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {AlunoRegisterComponent} from './aluno-register.component';
+
+const alunosRoutes: Routes = [
+  {path: 'register', redirectTo: '/alunoregister'},
+  {path: 'alunoregister', component: AlunoRegisterComponent}
+];
+
+@NgModule({
+  imports:[
+    RouterModule.forChild(alunosRoutes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AlunoRoutingModule{ }
