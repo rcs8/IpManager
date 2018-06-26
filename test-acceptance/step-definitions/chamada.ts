16445bda7fcc73580c6e6c2f3dae86c2e9a6d72c
Rodrigo Cunha <rcs8@cin.ufpe.br>
2018-06-26 09:31:53 -0300

    [#17] Initial testing (Error)

--- test-acceptance/step-definitions/chamada.ts
+++ test-acceptance/step-definitions/chamada.ts
@@ -14,7 +14,7 @@ defineSupportCode(function({Given,When,Then}){
     await browser.get("http://localhost:4200/");
     await expect (browser.getTitle()).to.eventually.equal('IpmGui');
     await $("a[name='classes']").click();
-  });
+  })
   Given(/^Given I have "([^\"]*)" registered as a student$/, async(loginCin) => {
     await $("a[name='cadastro']").click();
     await $("input[name='loginCin']").sendKeys(<string> loginCin);
@@ -26,21 +26,21 @@ defineSupportCode(function({Given,When,Then}){
     allLogins.filter(elem => sameLogin(elem, loginCin)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
 
     await $("a[name='classes']").click();
-  });
+  })
   Given(/^Given I have the class with day "([^\"]*)" and description "([^\"]*)" in the list$/, async(day, description) => {
     await $("input[name='day']").sendKeys(<string> day);
     await $("input[name='description']").sendKeys(<string> description);
     await element(by.buttonText('Cadastrar')).click();
 
     var allClasses : ElementArrayFinder = element.all(by.name('classesList'));
-    allClasses.filter(elem => sameName(elem, loginCin)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-  });
+    allClasses.filter(elem => sameName(elem, description)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+  })
   When(/^I try to submit "([^\"]*)" attendance$/, async(loginCin) => {
     await $("a[name='class']").click();
     await element(by.buttonText('Falta')).click();
-  });
+  })
   Then(/^The student "([^\"]*)" will have "(\d*)" abscence$/, async(loginCin, faltas) => {
     var allAlunos : ElementArrayFinder = element.all(by.name('alunolist'));
     allAlunos.filter(elem => pAND(sameLogin(elem, loginCin),sameAbsence(elem,faltas))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-  });
+  })
 })
---
+++ test-acceptance/typeScript/step-definitions/chamada.js
@@ -0,0 +1,50 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cucumber_1 = require("cucumber");
+const protractor_1 = require("protractor");
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+let sameLogin = ((elem, loginCin) => elem.element(protractor_1.by.name('loginCinlist')).getText().then(text => text === loginCin));
+let sameName = ((elem, classDesc) => elem.element(protractor_1.by.name('classesList')).getText().then(text => text === classDesc));
+let sameAbsence = ((elem, faltas) => elem.element(protractor_1.by.name('absenceList')).getText().then(text => text === faltas));
+let pAND = ((p, q) => p.then(a => q.then(b => a && b)));
+cucumber_1.defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^Given I am at the classes page$/, () => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.browser.get("http://localhost:4200/");
+        yield expect(protractor_1.browser.getTitle()).to.eventually.equal('IpmGui');
+        yield protractor_1.$("a[name='classes']").click();
+    }));
+    Given(/^Given I have "([^\"]*)" registered as a student$/, (loginCin) => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.$("a[name='cadastro']").click();
+        yield protractor_1.$("input[name='loginCin']").sendKeys(loginCin);
+        yield protractor_1.$("input[name='loginHuxley']").sendKeys(loginCin);
+        yield protractor_1.$("input[name='senha']").sendKeys(loginCin);
+        yield protractor_1.element(protractor_1.by.buttonText('Cadastrar')).click();
+        var allLogins = protractor_1.element.all(protractor_1.by.name('loginCinlist'));
+        allLogins.filter(elem => sameLogin(elem, loginCin)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        yield protractor_1.$("a[name='classes']").click();
+    }));
+    Given(/^Given I have the class with day "([^\"]*)" and description "([^\"]*)" in the list$/, (day, description) => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.$("input[name='day']").sendKeys(day);
+        yield protractor_1.$("input[name='description']").sendKeys(description);
+        yield protractor_1.element(protractor_1.by.buttonText('Cadastrar')).click();
+        var allClasses = protractor_1.element.all(protractor_1.by.name('classesList'));
+        allClasses.filter(elem => sameName(elem, description)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    }));
+    When(/^I try to submit "([^\"]*)" attendance$/, (loginCin) => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.$("a[name='class']").click();
+        yield protractor_1.element(protractor_1.by.buttonText('Falta')).click();
+    }));
+    Then(/^The student "([^\"]*)" will have "(\d*)" abscence$/, (loginCin, faltas) => __awaiter(this, void 0, void 0, function* () {
+        var allAlunos = protractor_1.element.all(protractor_1.by.name('alunolist'));
+        allAlunos.filter(elem => pAND(sameLogin(elem, loginCin), sameAbsence(elem, faltas))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    }));
+});
