11197edf75fa03b04bb42d1b3dd98528d5d34025
Tharcio <tharcio.0897@gmail.com>
2018-06-28 23:23:13 -0300

    atualizaçao no module para integraçao

--- ipm-gui/src/app/app.module.ts
+++ ipm-gui/src/app/app.module.ts
@@ -1,16 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+import { HttpModule } from '@angular/http';
 
+import { RelatorioService } from './relatorio/relatorio.service';
 import { AppComponent } from './app.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
+import { AlunoNotasComponent } from './aluno-notas/aluno-notas.component';
+import { AlocaçaoMonitoresComponent } from './alocaçao-monitores/alocaçao-monitores.component';
+import { MonitorComponent } from './monitor/monitor.component';
+import { AlunoComponent } from './aluno/aluno.component';
+import { AlunoService } from './aluno/aluno.service';
+import { AlocaçaoMonitoresService } from './alocaçao-monitores/alocaçao-monitores.service';
+import { TurmaAtualComponent} from './turma/turmaAtual.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelatorioComponent,
+    AlunoNotasComponent,
+    AlocaçaoMonitoresComponent,
+    MonitorComponent,
+    AlunoComponent,
+    TurmaAtualComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule, 
+    RouterModule.forRoot([
+      {
+        path: 'relatorio',
+        component: RelatorioComponent
+      },
+	  {
+		path: 'turmaAtual',
+        component: TurmaAtualComponent
+	  },
+      {
+        path: 'aluno-notas:loginCin',
+        component: AlunoNotasComponent
+      },
+	  {
+		path: 'alocacao',
+        component:AlocaçaoMonitoresComponent
+	  }
+    ])
   ],
-  providers: [],
+  providers: 
+	[RelatorioService,
+	AlunoService,
+	AlocaçaoMonitoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
