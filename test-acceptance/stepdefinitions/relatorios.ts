25f4f6f80538f71e4db0e11881591001d4c4ba60
Tharcio <tharcio.0897@gmail.com>
2018-06-29 00:03:22 -0300

    [#7] [#11]  teste de gui para relatorios turmas anteriores e turma atual

--- test-acceptance/stepdefinitions/relatorios.ts
+++ test-acceptance/stepdefinitions/relatorios.ts
@@ -5,10 +5,32 @@ let expect = chai.expect;
 
 let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
 
-let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
+let sameLogin = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
 let sameMedia = ((elem, media) => elem.element(by.name('medialist')).getText().then(text => text === media));
 let sameStatus = ((elem, stts) => elem.element(by.name('statuslist')).getText().then(text => text === stts));
 let samePresença = ((elem, presença) => elem.element(by.name('preslist')).getText().then(text => text === presença));
+
+let cadastrarAluno = ((login) => {
+    await $("a[name='cadastro']").click();
+    await $("input[name='loginCin']").sendKeys(<string> login);
+    await $("input[name='loginHuxley']").sendKeys(<string> login);
+    await $("input[name='password']").sendKeys(<string> login);
+    await element(by.buttonText('Cadastrar')).click();
+});
+
+let setNotas = ((aluno,lst1,lst2,lst3,lst4,lst5,lst6,av1,av2,av3,mp) => {
+	await $("a[name='notas']").click();
+	await $("input[name='lista1']").sendKeys(<string> lst1);
+	await $("input[name='lista2']").sendKeys(<string> lst2);
+	await $("input[name='lista3']").sendKeys(<string> lst3);
+	await $("input[name='lista4']").sendKeys(<string> lst4);
+	await $("input[name='lista5']").sendKeys(<string> lst5);
+	await $("input[name='lista6']").sendKeys(<string> lst6);
+	await $("input[name='EE1']").sendKeys(<string> av1);
+	await $("input[name='EE2']").sendKeys(<string> av2);
+	await $("input[name='EE1']").sendKeys(<string> av3);
+	await $("input[name='miniprova']").sendKeys(<string> mp);
+})
 
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
 
@@ -16,22 +38,19 @@ defineSupportCode(function ({ Given, When, Then }) {
     Given(/^Eu estou na pagina Relatorios Turma$/, async () => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('IpManager');
-        await $("a[name='RelatoriosTurma']").click();
+        await $("a[name='relatorio']").click();
     })
 	
-	Given(/^existe o aluno "([^\"]*)" com notas "(\d*)", "(\d*)", "(\d*)", "(\d*)" nas lista e "(\d*)", "(\d*)" nas provas, e "(\d*)" de presença $/,
-	async (nome, lst1,lst2,lst3,lst4,av1,av2,pr) => {
-		var aluno: new Aluno(nome);
-		aluno.setNotas(lst1,lst2,lst3,lst4,av1,av2);
-		aluno.setPresença(pr);
-		
+	Given(/^existe o aluno "([^\"]*)" com notas "(\d*)", "(\d*)", "(\d*)", "(\d*)", "(\d*)", "(\d*)" nas lista e "(\d*)", "(\d*)", "(\d*)" nas provas e "(\d*)" no mineprojeto $/,
+	async (nome, lst1,lst2,lst3,lst4,lst5,lst6,av1,av2,av3,mp) => {
+		cadastrarAluno(nome);
+		setNotas(lst1,lst2,lst3,lst4,lst5,lst6,av1,av2,av3,mp);		
 	});
 	
 	Given(/^Eu sou o aluno "([^\"]*)"$/, async(nome) => {
 		await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('IpManager');
-		await $("input[name='nomeLogin']").sendKeys(<string> nome);
-        await element(by.buttonText('Login')).click();
+		cadastrarAluno(nome);
 	})
 	
 	Given(/^tenho as notas "(\d*)", "(\d*)", "(\d*)", "(\d*)" nas lista e "(\d*)", "(\d*)" nas provas, e "(\d*)" de presença$/,
@@ -44,16 +63,16 @@ defineSupportCode(function ({ Given, When, Then }) {
 	})
 	
 	When(/^eu peço o relatorio de turmas anteriores$/, async () => {
-		await element(by.buttonText("Relatório de Turmas Antigas")).click();
+		await $("a[name='relatorio']").click();
 	});
 	
 	When(/^eu peço os relatorios da turma atual $/, async () => {
-		await element(by.buttonText("Relatório da Turma Atual")).click();
+		await $("a[name='turmaAtual']").click();
 	});
 	
 	Then(/^aparece o aluno "([^\"]*)", media "(\d*)", presença "(\d*)" e status "([^\"]*)"$/, async (nome,media,pres,stt) => {
 		var alunos : ElementArrayFinder = element.all(by.name('relatorioturma'));
-		alunos.filter(elem => pAND(pAND(sameName(elem,name),sameMedia(elem,media)),pAND(sameStatus(elem,stt),samePresença(elem,pres))));
+		alunos.filter(elem => pAND(pAND(sameLogin(elem,name),sameMedia(elem,media)),pAND(sameStatus(elem,stt),samePresença(elem,pres))));
 	});
 
 })
